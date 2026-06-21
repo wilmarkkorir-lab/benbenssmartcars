9b3f82f7f063838df8edb97b255c46558d7aea95
wilmarkkorir-lab <wilmarkkorir@gmail.com>
2026-06-21 16:48:52 +0300

    Update project

--- benbens_smart_cars/settings.py
+++ benbens_smart_cars/settings.py
@@ -25,7 +25,12 @@ SECRET_KEY = 'django-insecure-f68zo%s+3^p4*$v5t1o)7#3rtl(u#8$&6d!8bwbv)-&1t53r6g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'benbenssmartcars.alwaysdata.net',
+    'localhost',
+    '127.0.0.1',
+    '.alwaysdata.net',
+]
 
 
 # Application definition
@@ -122,7 +127,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
@@ -132,6 +138,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
     'http://localhost:3001',
+    'https://benbenssmartcars.alwaysdata.net',
 ]
 
 REST_FRAMEWORK = {
--- cars/views.py
+++ cars/views.py
@@ -9,11 +9,17 @@ class CategoryViewSet(viewsets.ModelViewSet):
 
 
 class CarViewSet(viewsets.ModelViewSet):
-    queryset = Car.objects.filter(is_available=True).order_by('-created_at')
     serializer_class = CarSerializer
     filter_backends = [filters.SearchFilter, filters.OrderingFilter]
     search_fields = ['brand', 'model', 'year', 'condition', 'category__name']
     ordering_fields = ['price', 'year', 'created_at']
+
+    def get_queryset(self):
+        queryset = Car.objects.all().order_by('-created_at')
+        available = self.request.query_params.get('is_available')
+        if available is None:
+            queryset = queryset.filter(is_available=True)
+        return queryset
 
 
 class CarImageViewSet(viewsets.ModelViewSet):
